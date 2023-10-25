188e27c0bc909d3496ca42b77e248b83b3a443d3
tin <let042003@gmail.com>
2023-10-25 08:46:41 +0700

    sprin2

---
+++ app/src/main/java/com/example/appbanvemaybay/IImageClickListener.java
@@ -0,0 +1,9 @@
+package com.example.appbanvemaybay;
+
+import android.view.View;
+
+public class IImageClickListener {
+    void onImageClick(View view, int pos, int giatri) {
+
+    }
+}
---
+++ app/src/main/java/com/example/appbanvemaybay/TinhTongEvent.java
@@ -0,0 +1,4 @@
+package com.example.appbanvemaybay;
+
+public class TinhTongEvent {
+}
